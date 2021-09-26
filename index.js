a2d91a2596ac276ac266fd74ce3e595ca0ece8d4
Harry <89729383+iHarryD@users.noreply.github.com>
2021-09-26 17:47:34 +0000

    added more questions

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const questionsAnswers = {
   "Who was the youngest homonculus in Fullmetal Alchemist?": "Wrath",
   "How many Hashiras were there in season one of Demon Slayer?": "9",
   "What other term did Mitsuha's teacher use for Twilight in Your Name?": "After Light",
-  "What family was Levi from in Attack on Titan?": "Ackerman"
+  "What family was Levi from in Attack on Titan?": "Ackerman",
+  "Who was the first to kill a homonculus in Fullmetal Alchemist?": "Mustang",
+  "What alias did L use when he met Light for the first time in Death Note": "Ryuzaki",
+  "What's the name of the Upper Three demon in Demon Slayer?": "Akaza"
 }
 
 let scoreboard = {};
